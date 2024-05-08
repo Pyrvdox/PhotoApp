3b21d46c4121e21021bb297f6fe34acc115ecc65
Pyrvdox <cierech8991@gmail.com>
2024-05-08 23:21:44 +0200

    login working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import axios from 'axios';
 import LoginPage from "./Pages/Login/loginpage";
 import LogoutPage from "./Pages/Logout/logoutpage";
 import MainApp from "./Pages/Main/mainapp";
 import RegisterPage from "./Pages/Register/registerpage";
 
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-axios.defaults.withCredentials = true;
 
-const client = axios.create({
-  baseURL: "http://127.0.0.1:5432"
-});
 
 
 const App = () => {
--- frontend/src/Pages/Login/loginform.js
+++ frontend/src/Pages/Login/loginform.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import client from '../../api';
 
 const LoginForm = () => {
   
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const navigate = useNavigate();
+
+  const loginFormHandler = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log('Login data:', loginData);
+      const resp = await client.post("api/login/",
+        {
+          username: loginData.username,
+          password: loginData.password
+        }
+      );
+      if (resp.status === 200) { 
+        console.log('Udane logowanie!');
+        navigate("/app")
+      }
+    }
+    catch (error){
+      console.error('Error:', error);
+    }
+    
+  };
+  
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
       <h1>Email</h1>
         <input
           type="text"
           id="username"
           name="username"
+          value={loginData.username}
+          onChange={loginFormHandler}
         />
       </div>
       <div>
@@ -18,6 +61,8 @@ const LoginForm = () => {
           type="password"
           id="password"
           name="password"
+          value={loginData.password}
+          onChange={loginFormHandler}
         />
       </div>
       <button type="submit">Login</button>

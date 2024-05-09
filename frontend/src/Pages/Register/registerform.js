1a82d19b1686f1fcd6ec4cbc1e1a8a9779ff9070
Pyrvdox <cierech8991@gmail.com>
2024-05-09 13:48:33 +0200

    register page

--- frontend/src/Pages/Register/registerform.js
+++ frontend/src/Pages/Register/registerform.js
@@ -1,15 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import React from 'react';
+import client from "../../utils/api";
 
 const RegisterForm = () => {
   
+  const navigate = useNavigate();
+
+  const [registerData, setRegisterData] = useState({
+    username: "",
+    password: "",
+    password2:""
+  })
+
+  const registerFormHandler = (e) => {
+    const { name, value } = e.target;
+    setRegisterData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      };
+    });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(registerData)
+      if (registerData.password === registerData.password2){
+        const resp = await client.post('api/register/',{
+          username: registerData.username,
+          password: registerData.password
+        })
+      }
+      if (resp.status === 200) { 
+        console.log('Udana rejestracja!');
+        navigate("/")
+      }
+      
+    }
+    catch (error){
+      console.error('Error:', error);
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
       <h1>Email</h1>
         <input
           type="text"
           id="username"
           name="username"
+          value={registerData.username}
+          onChange={registerFormHandler}
         />
       </div>
       <div>
@@ -18,6 +62,8 @@ const RegisterForm = () => {
           type="password"
           id="password"
           name="password"
+          value={registerData.password}
+          onChange={registerFormHandler}
         />
       </div>
       <div>
@@ -26,6 +72,8 @@ const RegisterForm = () => {
           type="password"
           id="password2"
           name="password2"
+          value={registerData.password2}
+          onChange={registerFormHandler}
         />
       </div>
       <button type="submit">Login</button>

8d62387bc52d4a3a3f9f03040567280ef57e0d87
Pyrvdox <cierech8991@gmail.com>
2024-05-10 22:28:23 +0200

    Footer Component
    Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,27 @@ import LogoutPage from "./Pages/Logout/logoutpage";
 import MainApp from "./Pages/Main/mainapp";
 import RegisterPage from "./Pages/Register/registerpage";
 import Navbar from "./Pages/Main/navbar";
-
+import Footer from "./Pages/Main/footer";
 
 
 const App = () => {
   return (
     <Router>
       <div>
+      <section>
         <Navbar />
+      </section>
+      <section>
         <Routes>
           <Route path="/" element={<LoginPage />}/>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/app" element={<MainApp />} />
           <Route path="/logout" element={<LogoutPage/>}/>
         </Routes>
-
+      </section>
+      <section>
+        <Footer />
+      </section>
       </div>
     </Router>
 
